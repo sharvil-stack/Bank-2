985316fe3960561ae7da481dd9b840ae7d6273e9
Sharvil Bhangre <sharvil.bhangre@gmail.com>
2026-06-01 20:06:16 +0530

    implement JWT authentication and secure protected endpoints

--- src/main/java/org/project/bank2/config/SecurityConfig.java
+++ src/main/java/org/project/bank2/config/SecurityConfig.java
@@ -1,22 +1,30 @@
 package org.project.bank2.config;
 
+import org.project.bank2.security.JwtAuthFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder()
     {
+
         return new BCryptPasswordEncoder();
     }
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(
@@ -25,8 +33,12 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth ->
-                        auth.anyRequest().permitAll()
-                );
+                        auth.requestMatchers("/auth/login",
+                                "/auth/register").permitAll()
+                        .anyRequest().authenticated()
+
+                ).sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
---
+++ src/main/java/org/project/bank2/security/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package org.project.bank2.security;
+
+import org.project.bank2.model.User;
+import org.project.bank2.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepo userRepo;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(email).orElseThrow(()->new UsernameNotFoundException("Invalid email or password"));
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
+
+
+
+}
+
