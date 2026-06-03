4305e958560c6fd7f5292cc879d05d3d6848b87b
Sharvil Bhangre <sharvil.bhangre@gmail.com>
2026-06-03 22:26:23 +0530

    Frontend Setup completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+
+function App() {
+  return (
+    <h1>Bank2 Frontend</h1>
+  )
+}
+
+export default App
