4ed598bbaf3181789e29830d820da9a0bbadf0cf
Sharvil Bhangre <sharvil.bhangre@gmail.com>
2026-06-12 00:27:32 +0530

    Fixed CORS

--- src/main/java/org/project/bank2/config/SecurityConfig.java
+++ src/main/java/org/project/bank2/config/SecurityConfig.java
@@ -34,9 +34,8 @@ public class SecurityConfig {
         CorsConfiguration configuration =
                 new CorsConfiguration();
 
-        configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
-        );
+        configuration.setAllowedOriginPatterns( List.of("*") );
+
 
         configuration.setAllowedMethods(
                 List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")
