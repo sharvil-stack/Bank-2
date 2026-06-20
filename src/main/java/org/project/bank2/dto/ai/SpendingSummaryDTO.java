740a4b3ff7d84b2694c22ad3250c7dcd009895ad
Sharvil Bhangre <sharvil.bhangre@gmail.com>
2026-06-20 20:11:45 +0530

    Fixed aiAssisstantService

--- src/main/java/org/project/bank2/dto/ai/SpendingSummaryDTO.java
+++ src/main/java/org/project/bank2/dto/ai/SpendingSummaryDTO.java
@@ -1,4 +1,12 @@
 package org.project.bank2.dto.ai;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
 public class SpendingSummaryDTO {
+    private Map<String,Double> categories;
 }
